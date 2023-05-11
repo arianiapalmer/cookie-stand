9eb88d68d8fd3b5c82e5596ae21d861821e352ad
arianiapalmer <arianiapalmer@gmail.com>
2023-05-10 23:05:55 -0400

    added cookies per hr method - still needs test

--- js/app.js
+++ js/app.js
@@ -1,42 +1,79 @@
 'use strict';
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+// let storeHourlyTotals = [];
 let seattle = {
   minCust: 23,
   maxCust:65,
   avgCookieSale: 6.3,
+  storeHourlyTotals: [],
   custPerHr: function(){
     return Math.floor(Math.random() * (10 - 0) + 0);
+  },
+  cookiesPerHr: function(){
+    for(let i=0; i<hours.length; i++){
+      let hourSale = this.custPerHr()*this.avgCookieSale; 
+      this.storeHourlyTotals.push(hourSale);
+    }
   }
 };
 let tokyo = {
   minCust: 3,
   maxCust: 24,
   avgCookieSale: 1.2,
+  storeHourlyTotals: [],
   custPerHr: function(){
     return Math.floor(Math.random() * (10 - 0) + 0);
-  }
+  },
+  cookiesPerHr: function(){
+    for(let i=0; i<hours.length; i++){
+      let hourSale = this.custPerHr()*this.avgCookieSale; 
+      this.storeHourlyTotals.push(hourSale);
+    }
+  },
 };
 let dubai = {
   minCust: 11,
   maxCust: 38,
   avgCookieSale: 3.7,
+  storeHourlyTotals: [],
   custPerHr: function(){
     return Math.floor(Math.random() * (10 - 0) + 0);
-  }
+  },
+  cookiesPerHr: function(){
+    for(let i=0; i<hours.length; i++){
+      let hourSale = this.custPerHr()*this.avgCookieSale; 
+      this.storeHourlyTotals.push(hourSale);
+    }
+  },
 };
 let paris = {
   minCust: 20,
   maxCust: 38,
   avgCookieSale: 2.3,
+  storeHourlyTotals: [],
   custPerHr: function(){
     return Math.floor(Math.random() * (10 - 0) + 0);
+  },
+  cookiesPerHr: function(){
+    for(let i=0; i<hours.length; i++){
+      let hourSale = this.custPerHr()*this.avgCookieSale; 
+      this.storeHourlyTotals.push(hourSale);
+    }
   }
 };
 let london = {
   minCust: 2,
   maxCust: 16,
   avgCookieSale: 4.6,
+  storeHourlyTotals: [],
   custPerHr: function(){
     return Math.floor(Math.random() * (10 - 0) + 0);
+  },
+  cookiesPerHr: function(){
+    for(let i=0; i<hours.length; i++){
+      let hourSale = this.custPerHr()*this.avgCookieSale; 
+      this.storeHourlyTotals.push(hourSale);
+    }
   }
 };
+
